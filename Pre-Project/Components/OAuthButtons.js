dde327c9bc22d8d96690671a4568bb2a7e5ea077
HooniKim <tpgns2289@naver.com>
2023-02-21 23:41:37 +0900

    21일 작업물

--- Pre-Project/Components/OAuthButtons.js
+++ Pre-Project/Components/OAuthButtons.js
@@ -1,28 +1,22 @@
 import styled from 'styled-components';
-import {
-  Googlelogo,
-  GitHublogo,
-  Facebooklogo
-} from '../../assets/Login_assets/Logos';
+import { GoogleSVG, GitHubSVG, FacebookSVG } from '../../assets/LoginSVG';
 
 const ButtonWrapper = styled.button`
   background-color: ${({ bgColor }) => bgColor};
   color: ${({ color }) => color};
-  border: 1px solid gainsboro;
-  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0);
+  padding: 10.4px;
+  border-radius: 3px;
   width: 100%;
   height: ${({ height }) => height};
   cursor: pointer;
-  transition: background-color 0.2s;
-  font-size: 15px;
+  font-size: 13px;
   &:hover {
     background-color: ${({ hoverBgColor }) => hoverBgColor};
   }
-  .icon {
-    margin-right: 10px;
-  }
-  ${({ isLoginButton }) =>
-    isLoginButton && `box-shadow: 1px 2px 5px rgba(0, 0, 0, 0.25);`}
+  ${({ GeneralBtnShadow }) =>
+    GeneralBtnShadow &&
+    `box-shadow: rgba(255, 255,255, 0.4) 0px 1px 0px 0px inset;`}
   .span-style {
     display: inline-flex;
     align-items: center;
@@ -30,69 +24,68 @@ const ButtonWrapper = styled.button`
   }
 `;
 
-const GoogleButton = () => {
+const GoogleBtn = () => {
   return (
     <ButtonWrapper
       bgColor='#fff'
       color='#000'
       hoverBgColor='#eee'
       activeBgColor='#ddd'
-      height='42px'
+      height='38px'
     >
       <span className='span-style'>
-        <Googlelogo className='icon' />
+        <GoogleSVG className='icon' />
         Log in with Google
       </span>
     </ButtonWrapper>
   );
 };
 
-const GithubButton = () => {
+const GithubBtn = () => {
   return (
     <ButtonWrapper
       bgColor='#222'
       color='#fff'
       hoverBgColor='#000'
       activeBgColor='#333'
-      height='42px'
+      height='38px'
     >
       <span className='span-style'>
-        <GitHublogo className='icon' /> Log in with GitHub
+        <GitHubSVG className='icon' /> Log in with GitHub
       </span>
     </ButtonWrapper>
   );
 };
 
-const FacebookButton = () => {
+const FacebookBtn = () => {
   return (
     <ButtonWrapper
       bgColor='#3b5998'
       color='#fff'
       hoverBgColor='#2f477a'
       activeBgColor='#293e69'
-      height='42px'
+      height='38px'
     >
       <span className='span-style'>
-        <Facebooklogo className='icon' />
-        Log in with Facebook
+        <FacebookSVG className='icon' /> Log in with Facebook
       </span>
     </ButtonWrapper>
   );
 };
 
-const LoginButton = () => {
+const GeneralBtn = ({ BtnText = 'Log in', bgColor, height }) => {
   return (
     <ButtonWrapper
-      bgColor='#0895ff'
-      color='#fff'
+      bgColor={bgColor || 'rgb(10, 149, 255)'}
+      color='rgb(255, 255, 255)'
       hoverBgColor='#2f477a'
       activeBgColor='#293e69'
-      height='42px'
-      isLoginButton
+      height={height || '38px'}
+      GeneralBtnShadow
     >
-      Log in
+      {BtnText}
     </ButtonWrapper>
   );
 };
 
-export { GoogleButton, GithubButton, FacebookButton, LoginButton };
+export { GoogleBtn, GithubBtn, FacebookBtn, GeneralBtn };
