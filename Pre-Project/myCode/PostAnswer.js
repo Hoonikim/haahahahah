97226e189b7877199032b39d9bdcfa23fffbead2
HooniKim <tpgns2289@naver.com>
2023-03-01 23:37:13 +0900

    5

--- Pre-Project/myCode/PostAnswer.js
+++ Pre-Project/myCode/PostAnswer.js
@@ -117,7 +117,7 @@ export default PostAnswer;
     margin-bottom: 15px;
     border: 1px solid #b1b7bc; */
 /* &:focus {
-      border: 1px solid #409ad6;
+      border: 1px solid #409ad6; 
       box-shadow: 0 0 0 4px #d9e9f6;
       outline: none;
     } */
